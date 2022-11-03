bbbf24b5ceeae9342c0053af9c187af96ad78b29
Yuuuk94 <yuuuk94@gmail.com>
2022-11-04 02:01:18 +0900

    style. about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -18,24 +18,137 @@ const About: NextPage = () => {
           &nbsp;&nbsp;yuuuk94@gmail.com
         </p>
         <AnimetionImg />
-        <p>
-          제가 생각하는 프론트엔드 개발은 사용자가 가장 먼저 만나는
-          “얼굴”입니다.
+        <p className="text-md text-textMain font-light leading-loose">
+          제가 생각하는{" "}
+          <span className="font-bold">
+            프론트엔드 개발은 사용자가 가장 먼저 만나는 “얼굴”
+          </span>
+          입니다.
           <br />
-          JAVASCRIPT와 REACT를 기반으로 작업 중인 프론트엔드 개발자,
-          신유경입니다.
+          사용자와 양방향 소통할 수 있는{" "}
+          <span className="font-bold">인터렉션 기능</span>에 관심이 많습니다.
           <br />
-          사용자와 양방향 소통할 수 있는 인터렉션 기능에 관심이 많습니다.
-          사용자의 행동을 유도하는 UI/UX가 어떻게 하면 유의미하게 비즈니스로
-          연결될 수 있을지 고민합니다. 실제로 서비스 되는 웹과 앱을 분석하고,
-          직접 서비스를 개발해보며 성장해가고 있습니다.
+          <span className="font-bold">사용자의 행동을 유도하는 UI/UX</span>가
+          어떻게 하면{" "}
+          <span className="font-bold">
+            유의미한 비즈니스로 연결되어 가치 창출
+          </span>
+          할 수 있을지 고민 합니다.
           <br />
-          개발뿐만 아니라 기획과 디자인에도 관심이 많고 이해도가 높습니다.
-          다양한 분야의 사람들과 소통하기를 좋아합니다.
+          <span className="font-bold">실제 서비스를 분석</span>
+          하고, 다양한{" "}
+          <span className="font-bold">
+            프레임워크와 라이브러리를 시도
+          </span>하며 <span className="font-bold">꾸준한 성장을 위해 노력</span>
+          하고 있습니다.
+          <br />
+          개발뿐만 아니라 <span className="font-bold">기획과 디자인</span>에도
+          관심이 많고 이해도가 높습니다.
+          <br />
+          동료들과의{" "}
+          <span className="font-bold">목표를 위한 효율적인 커뮤니케이션</span>을
+          지향합니다.
         </p>
       </NeoContent>
       <NeoContent>
-        <div>기술 스택</div>
+        <div>
+          <p className="pb-6 text-lg font-semibold leading-snug text-textMain">
+            Experience
+          </p>
+          <div className="text-textMain">
+            <p className="mb-2 text-lg font-bold">유비케어</p>
+            <p className="text-sm">플랫폼 개발팀 / 프론트엔드 엔지니어</p>
+            <p className="text-sm">2022. 07 ~ 2022. 09 </p>
+            <br />
+            <p className="mb-2 font-bold">- 의사 경영 지원 플랫폼</p>
+            <p className="mb-4 text-sm leading-relaxed">
+              병원과 의사 경영에 필요한 다양한 서비스를 연결해주는 웹 기반
+              플랫폼 서비스
+              <br /> Vuetify가 적용된 UI/UX를 디자인에 맞게 커스텀하여 회원가입
+              및 다수 서비스 화면 구현
+              <br />
+              Js와 iframe으로 연동되는 외부 서비스를 Vue에 맞게 적용하여 자사
+              플랫폼에 연동
+              <br />뷰 로직과 비즈니스 로직의 분리를 고민하며, 구현된 서비스
+              웹사이트 보수
+              <br /> git flow를 도입한 개발 전략에 따라 git과 sourcetree를
+              활용하여 팀원들과 협업
+              <br /> 기획자, 디자이너와 적극적으로 커뮤니케이션하며, 인터렉션
+              구현 및 UI/UX 개선
+              <br />
+              Vue, Vuetify, CSS, Git, ESLint, Prettier, Zepline, Redmine
+            </p>
+            <p className="mb-2 font-bold">- 플랫폼 개발팀 내 세미나 참여</p>
+            <p className="mb-4 text-sm leading-relaxed">
+              팀 내 세미나에서 "사용자 이탈을 방지하는 UX 아이디어"와
+              "리팩터링"에 대해 발표
+            </p>
+            <p className="mb-2 font-bold">- 고객센터 콜백 접수 서비스</p>
+            <p className="mb-4 text-sm leading-relaxed">
+              입사 후 처음 참여한 고객센터로부터 콜백 요청하는 웹 기반 서비스
+              <br />
+              고객센터의 연결 상태 화면 뷰와 공지사항 및 검색 화면 뷰 구현
+              <br />
+              Nuxt.js, Vuetify, SCSS, Git, ESLint, Prettier
+            </p>
+          </div>
+          <p></p>
+        </div>
+      </NeoContent>
+      <NeoContent>
+        <div>
+          <p className="pb-6 text-lg font-semibold leading-snug text-textMain">
+            Other Experience
+          </p>
+          <div className="text-textMain">
+            <p className="mb-2 text-lg font-bold">
+              주니어 프론트엔드 개발자 대상 프리온보딩 챌린지
+            </p>
+            <p className="text-sm">주관사 원티드랩 </p>
+            <p className="text-sm">2022. 10</p>
+            <p className="mb-2 text-sm leading-relaxed">
+              React+TypeScript로 Context API, History API를 활용하여 Routing
+              기능 구현 <br />
+              CSR, SSR, SSG에 대해 학습
+              <br />
+              Next.js로 SSG 정적 블로그 구현 및 최적화
+            </p>
+          </div>
+          <br />
+          <div className="text-textMain">
+            <p className="mb-2 text-lg font-bold">
+              [NCS]프론트엔드 개발 양성과정 수료
+            </p>
+            <p className="text-sm">그린 컴퓨터 아카데미</p>
+            <p className="text-sm">2021. 11 ~ 2022. 04</p>
+            <p className="mb-2 text-sm leading-relaxed">
+              React, jQuery 등의 라이브러리와 프레임워크 활용한 화면 및 동적
+              기능 구현
+              <br />
+              Axios, Ajax, Fetch API를 통한 비동기 처리 및 외부 API 적용
+              <br />
+              My SQL, Firebase, PHP 등을 활용한 데이터베이스 구성 및 CRUD 구현
+            </p>
+          </div>
+        </div>
+      </NeoContent>
+      <NeoContent>
+        <div>
+          <p className="pb-6 text-lg font-semibold leading-snug text-textMain">
+            Skills
+          </p>
+          <p className="mb-2 text-sm text-textMain leading-relaxed">
+            React / Vue / TypeScript / Next.js
+            <br />
+            HTML / CSS / SCSS / JavaScript / jQuery
+            <br />
+            MySQL / Maria DB / Firebase / PHP
+            <br />
+            Git / GitHub
+            <br />
+            Zeplin / XD / Adobe Photoshop
+          </p>
+        </div>
       </NeoContent>
     </Layout>
   );
